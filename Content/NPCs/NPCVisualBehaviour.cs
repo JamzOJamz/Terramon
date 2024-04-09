ab06f4b9546beac4ef20f7b376d1046206979d56
Infina <49457443+InfinaMii@users.noreply.github.com>
2024-04-09 17:22:02 +0100

    Add NPCVisualBehaviour
    - Add NPCVisualBehaviour to control additional attributes given to Pokemon
    - Add LightColor, LightStrength to control light given off by a pokemon
    - Add DamperAmount to reduce the amount of light a Pokemon gives off when the Pokemon is wet or it is raining
    - Add DustID, DustFrequency and DustPosition to control dust pokemon spawns on itself
    - Modify Charmander.hjson to include these features

---
+++ Content/NPCs/NPCVisualBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using Terramon.Content.NPCs.Pokemon;
+using Terramon.Core.NPCComponents;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+// ReSharper disable UnassignedField.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ConvertToConstant.Global
+
+namespace Terramon.Content.NPCs;
+
+/// <summary>
+///     A <see cref="NPCComponent" /> to add variants to Pokemon NPCs.
+/// </summary>
+public class NPCVisualBehaviour : NPCComponent
+{
+    //TODO: Vector3 (or Color) and Vector2 support in hjson
+    public Vector3 LightColor = Vector3.One;
+    public float LightStrength = 0f;
+    public float DamperAmount = 0; //0 = no effect, 1 = full effect
+
+    public int DustID = -1;
+    public float DustFrequency = 20;//how many frames until dust is spawned
+    public Vector2 DustPosition;
+
+    float dustTimer;
+    Vector3 lightColor = Vector3.Zero;
+
+    public override void AI(NPC npc)
+    {
+        base.AI(npc);
+
+        if (LightStrength > 0)
+            Lighting.AddLight(npc.position, LightColor * LightStrength * (Main.raining || npc.wet ? 1 - DamperAmount : 1));
+
+        if (DustID > -1)
+        {
+            if (dustTimer >= DustFrequency)
+            {
+                Dust.NewDust(npc.position + DustPosition, 1, 1, DustID);
+                dustTimer = 0;
+            }
+            else
+                dustTimer++;
+        }
+    }
+}
