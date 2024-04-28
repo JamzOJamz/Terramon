f58b5fa246e35732909e0088b5614aa4b6f2e78f
JamzOhJamz <87213748+nsfury@users.noreply.github.com>
2024-04-28 14:24:47 -0600

    Update MusicTile.cs

--- Content/Tiles/MusicBoxes/MusicTile.cs
+++ Content/Tiles/MusicBoxes/MusicTile.cs
@@ -1,5 +1,6 @@
 using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ObjectData;
 using Terraria.Utilities;
 
@@ -8,27 +9,31 @@ namespace Terramon.Content.Tiles.MusicBoxes;
 public abstract class MusicTile : ModTile
 {
     public override string Texture => "Terramon/Assets/Tiles/MusicBoxes/" + GetType().Name;
-    
+
     public override void SetStaticDefaults()
     {
         Main.tileFrameImportant[Type] = true;
         Main.tileObsidianKill[Type] = true;
+        TileID.Sets.HasOutlines[Type] = true;
+        TileID.Sets.DisableSmartCursor[Type] = true;
+
         TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
         TileObjectData.newTile.Origin = new Point16(0, 1);
         TileObjectData.newTile.LavaDeath = false;
         TileObjectData.newTile.DrawYOffset = 2;
+        TileObjectData.newTile.StyleLineSkip = 2;
         TileObjectData.addTile(Type);
         DustType = DustID.Titanium;
 
-        var name = CreateMapEntryName();
-        AddMapEntry(new Color(200, 200, 200), name);
+        AddMapEntry(new Color(191, 142, 111), Language.GetText("ItemName.MusicBox"));
     }
-    
+
     public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
     {
         if (Lighting.UpdateEveryFrame && new FastRandom(Main.TileFrameSeed).WithModifier(i, j).Next(4) != 0) return;
-        if (Main.tile[i, j].TileFrameX != 36 || (int)Main.timeForVisualEffects % 7 != 0 ||
-            !Main._rand.NextBool(5)) return;
+        var tile = Main.tile[i, j];
+        if (tile.TileFrameX != 36 || tile.TileFrameY % 36 != 0 || (int)Main.timeForVisualEffects % 7 != 0 ||
+            !Main._rand.NextBool(3)) return;
         var MusicNote = Main._rand.Next(570, 573);
         Vector2 SpawnPosition = new(i * 16 + 8, j * 16 - 8);
         Vector2 NoteMovement = new(Main.WindForVisuals * 2f, -0.5f);
@@ -44,6 +49,6 @@ public abstract class MusicTile : ModTile
                 break;
         }
 
-        Gore.NewGore(new EntitySource_Misc(""), SpawnPosition, NoteMovement, MusicNote, 0.8f);
+        Gore.NewGore(new EntitySource_TileUpdate(i, j), SpawnPosition, NoteMovement, MusicNote, 0.8f);
     }
 }
