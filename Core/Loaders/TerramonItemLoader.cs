290f189998e7ab968d73ddbe67788e52cb32de6f
JamzOhJamz <87213748+nsfury@users.noreply.github.com>
2024-05-20 20:53:48 -0600

    Get all loadable types in TerramonItemLoader

--- Core/Loaders/TerramonItemLoader.cs
+++ Core/Loaders/TerramonItemLoader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using Terramon.Content.Items;
+using Terraria.ModLoader.Core;
 
 namespace Terramon.Core.Loaders;
 
@@ -8,15 +9,14 @@ public class TerramonItemLoader : ModSystem
 {
     public override void OnModLoad()
     {
-        var items = (from t in Mod.Code.GetTypes() where !t.IsAbstract && t.IsSubclassOf(typeof(TerramonItem)) select (TerramonItem)Activator.CreateInstance(t, null)).ToList();
+        var items = (from t in AssemblyManager.GetLoadableTypes(Mod.Code)
+            where !t.IsAbstract && t.IsSubclassOf(typeof(TerramonItem))
+            select (TerramonItem)Activator.CreateInstance(t, null)).ToList();
 
         // Sort items by the priority property
         var sortedItems = items.OrderBy(item => item.LoadPriority);
 
         // Add sorted items to the mod content
-        foreach (var item in sortedItems)
-        {
-            Mod.AddContent(item);
-        }
+        foreach (var item in sortedItems) Mod.AddContent(item);
     }
 }
