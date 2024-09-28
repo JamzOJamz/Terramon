8925936b2d45f45a423b427a027c648b4e226635
Infina <49457443+InfinaMii@users.noreply.github.com>
2024-09-28 23:32:10 +0100

    Improve Poke Ball tiles
    - Change right-click behaviour - each click with a matching item increases contents stack by one rather than consuming entire stack
    - Add amount indicator to cursor tooltips
    - Disable cursor tooltips if pokeball is disposable
    - Add TryOpen method for determining whether tiles should be destroyed or opened
    - Add SyncPkballTileRpc packet to be sent whenever tile contents are modified
    - Rename pkballTile to pkballItem in save/load data
    - Update bulbasaur minisprite

---
+++ Content/Packets/SyncPkballTileRpc.cs
@@ -0,0 +1,64 @@
+using EasyPacketsLib;
+using System.IO;
+using MonoMod.Utils;
+using Terramon.Content.Items.PokeBalls;
+using Terramon.Helpers;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace Terramon.Content.Packets;
+
+/// <summary>
+///     A packet to be sent whenever the contents of a Poké Ball tile are modified.
+/// </summary>
+public readonly struct SyncPkballTileRpc(Item myItem, bool isOpen, bool isDisposable, byte player, Point16 tileCoords)
+    : IEasyPacket<SyncPkballTileRpc>, IEasyPacketHandler<SyncPkballTileRpc>
+{
+    private readonly Item _item = myItem;
+    private readonly bool _isOpen = isOpen;
+    private readonly bool _isDisposable = isDisposable;
+    
+    private readonly byte _player = player;
+    private readonly Point16 _tileCoords = tileCoords;
+
+    public void Serialise(BinaryWriter writer)
+    {
+        _item.Serialize(writer, ItemSerializationContext.Syncing);
+        writer.Write(_isOpen);
+        writer.Write(_tileCoords.X);
+        writer.Write(_tileCoords.Y);
+    }
+
+    public SyncPkballTileRpc Deserialise(BinaryReader reader, in SenderInfo sender)
+    {
+        Item i = new Item();
+        i.DeserializeFrom(reader, ItemSerializationContext.Syncing);
+        return new SyncPkballTileRpc(i, reader.ReadBoolean(), reader.ReadBoolean(), reader.ReadByte(), new Point16(reader.ReadInt16(), reader.ReadInt16()));
+    }
+
+    public void Receive(in SyncPkballTileRpc packet, in SenderInfo sender, ref bool handled)
+    {
+        sender.Mod.Logger.Debug(
+            $"Received SyncPkballTileRpc on {(Main.netMode == NetmodeID.Server ? "server" : "client")}");// for player {packet._player}");
+        if (Main.netMode == NetmodeID.MultiplayerClient)
+        {
+            var tile = TileUtils.TryGetTileEntityAs<BasePkballEntity>(packet._tileCoords.X, packet._tileCoords.Y, out var e);
+            e.Item = myItem;
+            e.Disposable = packet._isDisposable;
+            
+            if (packet._isOpen)
+                e.TryOpen();
+            else
+                e.Open = true;
+            
+            var player = Main.player[packet._player];
+            player.itemRotation = 0;
+            player.SetItemAnimation(18);
+            
+            SoundEngine.PlaySound(SoundID.Mech, packet._tileCoords.ToWorldCoordinates());
+        }
+
+        handled = true;
+    }
+}
