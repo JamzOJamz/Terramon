03400a38e0d970033df7355191ec2adbd15d37bb
JamzOhJamz <abstractdeveloperx@gmail.com>
2024-10-11 23:32:24 -0600

    Update InventoryParty.cs

--- Content/GUI/InventoryParty.cs
+++ Content/GUI/InventoryParty.cs
@@ -86,7 +86,7 @@ public class InventoryParty : SmartUIState
             var slot = new CustomPartyItemSlot(i);
             if (reducedMotion) slot.Color = Color.White * 0f;
             CustomSlots[i] = slot;
-            AddElement(slot, slotPositionsX[i] - (reducedMotion ? 47 : 0), 254, 52, 52, container);
+            AddElement(slot, slotPositionsX[i] - (reducedMotion ? 47 : 0), 254, 48, 48, container);
         }
 
         Append(container);
