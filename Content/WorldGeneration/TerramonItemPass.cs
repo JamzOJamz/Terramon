229e84d797ef3ade5b3441a77455f2a901227628
JamzOhJamz <abstractdeveloperx@gmail.com>
2025-01-29 21:29:44 -0700

    Chest loot system overhaul

--- Content/WorldGeneration/TerramonItemPass.cs
+++ Content/WorldGeneration/TerramonItemPass.cs
@@ -1,4 +1,5 @@
 using Terramon.Content.Items;
+using Terramon.Content.Items.PokeBalls;
 using Terramon.Helpers;
 using Terraria.IO;
 using Terraria.Localization;
@@ -12,14 +13,27 @@ public class TerramonItemPass(string name, float loadWeight) : GenPass(name, loa
     {
         progress.Message = Language.GetTextValue("Mods.Terramon.WorldGen.ItemsPass");
 
+        AddChestLoot_PokeBalls();
+        AddChestLoot_RareCandies();
+    }
+
+    private static void AddChestLoot_RareCandies()
+    {
         var rareCandyType = ModContent.ItemType<RareCandy>();
-        ChestGen.AddChestLoot(rareCandyType, ChestID.Gold, 1, 3, 7500);
-        ChestGen.AddChestLoot(rareCandyType, minimumStack: 1, maximumStack: 2, chance: 3500,
+        ChestGen.AddChestLoot(rareCandyType, ChestID.Gold, 1, 3, 0.75f);
+        ChestGen.AddChestLoot(rareCandyType, minimumStack: 1, maximumStack: 2, chance: 0.35f,
             excludeDuplicates: true);
-        ChestGen.AddChestLoot(ModContent.ItemType<WaterStone>(), ChestID.Water, chance: 8500);
-        ChestGen.AddChestLoot(ModContent.ItemType<ThunderStone>(), ChestID.Gold_Locked, chance: 500);
-        ChestGen.AddChestLoot(ModContent.ItemType<FireStone>(), ChestID.Shadow_Locked, chance: 1000);
-        ChestGen.AddChestLoot(ModContent.ItemType<LeafStone>(), ChestID.LivingTrees, chance: 1500);
-        ChestGen.AddChestLoot(ModContent.ItemType<MoonStone>(), ChestID.Gold, chance: 250);
+        ChestGen.AddChestLoot(ModContent.ItemType<WaterStone>(), ChestID.Water, chance: 0.85f);
+        ChestGen.AddChestLoot(ModContent.ItemType<ThunderStone>(), ChestID.Gold_Locked, chance: 0.05f);
+        ChestGen.AddChestLoot(ModContent.ItemType<FireStone>(), ChestID.Shadow_Locked, chance: 0.1f);
+        ChestGen.AddChestLoot(ModContent.ItemType<LeafStone>(), ChestID.LivingTrees, chance: 0.15f);
+        ChestGen.AddChestLoot(ModContent.ItemType<MoonStone>(), ChestID.Gold, chance: 0.025f);
+    }
+
+    private static void AddChestLoot_PokeBalls()
+    {
+        ChestGen.AddChestLoot(ModContent.ItemType<PokeBallItem>(),
+            chest => chest.y < Main.worldSurface && Main.tile[chest.x, chest.y].TileFrameX / 36 == ChestID.Default, 3,
+            5, 0.5f);
     }
 }
