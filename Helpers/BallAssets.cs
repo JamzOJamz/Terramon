d193a79e1c8f7af7665d90d9b9a5627acefa59f2
JamzOhJamz <abstractdeveloperx@gmail.com>
2025-02-23 13:14:35 -0700

    Update BallAssets.cs

--- Helpers/BallAssets.cs
+++ Helpers/BallAssets.cs
@@ -3,27 +3,30 @@ using Terramon.ID;
 
 namespace Terramon.Helpers;
 
+[Autoload(Side = ModSide.Client)]
 public class BallAssets : ILoadable
 {
-    private const string PokeballIconPrefix = "Terramon/Assets/Items/PokeBalls/";
+    private const string PokeballIconPathFormat = "Terramon/Assets/Items/PokeBalls/{0}MiniItem";
     private static Asset<Texture2D>[] _ballIcons;
 
-    public static Asset<Texture2D> GetBallIcon(BallID id) => _ballIcons[(int)id];
-    
     public void Load(Mod mod)
     {
-        _ballIcons = new Asset<Texture2D>[byte.MaxValue + 1];
-        for (var i = 0; i <= byte.MaxValue; i++)
+        _ballIcons = new Asset<Texture2D>[256];
+        foreach (var ballId in Enum.GetValues(typeof(BallID)).Cast<BallID>())
         {
-            var ballName = Enum.GetName(typeof(BallID), i);
-            if (ballName == null) continue;
-            var ballIconName = ballName + "MiniItem";
-            _ballIcons[i] = ModContent.Request<Texture2D>(PokeballIconPrefix + ballIconName);
+            var id = (int)ballId;
+            var path = string.Format(PokeballIconPathFormat, ballId);
+            _ballIcons[id] = ModContent.Request<Texture2D>(path);
         }
     }
 
     public void Unload()
     {
         _ballIcons = null;
+    }
+
+    public static Asset<Texture2D> GetBallIcon(BallID id)
+    {
+        return _ballIcons[(int)id];
     }
 }
