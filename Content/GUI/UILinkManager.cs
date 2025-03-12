277492a4ec52540f6105f1ff39bab72ad9b962d4
Infina <49457443+InfinaMii@users.noreply.github.com>
2025-03-12 13:59:25 +0000

    Add TerramonPageID and TerramonPointID
    - Add TerramonPageID and TerramonPointID for storing page and point IDs respectively
    - Switch to using TerramonPageID, TerramonPointID, GamepadPageID and GamepadPointID rather than direct integer values

--- Content/GUI/UILinkManager.cs
+++ Content/GUI/UILinkManager.cs
@@ -46,42 +46,42 @@ public class UILinkManager : ILoadable
                     for (int i = 43; i <= 48; i++)
                         invPage.LinkMap[i - slotOffset].Down = 9600 + i - 43;
                 
-                //set nav input for collapse button
+                //set nav input for collapse button (didn't seem necessary to use GamepadPointID for inventory slots)
                 var collapseButtonPoint = (reducedMotion || compressedState) ? 48 : 42;
                 if (hasAutoTrash) collapseButtonPoint -= 1;
-                invPage.LinkMap[collapseButtonPoint].Down = 9606;
+                invPage.LinkMap[collapseButtonPoint].Down = TerramonPointID.PartyCollapse;
 
                 //set slot 47 regardless of autotrash/reduced motion (would otherwise go to bestiary)
                 if (compressedState)
-                    invPage.LinkMap[47].Down = 9606;
+                    invPage.LinkMap[47].Down = TerramonPointID.PartyCollapse;
                 
                 //set navigation from bestiary button to inventoryparty items (rather than inventory slot 47)
-                invPage.LinkMap[310].Up = compressedState || reducedMotion ? 9606 : 9605;
+                invPage.LinkMap[GamepadPointID.BestiaryMenu].Up = compressedState || reducedMotion ? TerramonPointID.PartyCollapse : TerramonPointID.Party5;
 
                 //set nav input for Pokédex icon
                 if (Main.GameModeInfo.IsJourneyMode)
                 {
                     if (hasAutoTrash)
                     {
-                        invPage.LinkMap[311].Down = 9607;
-                        invPage.LinkMap[311].Right = reducedMotion ? 9600 : 9606;
+                        invPage.LinkMap[GamepadPointID.CreativeMenuToggle].Down = TerramonPointID.HubUI;
+                        invPage.LinkMap[GamepadPointID.CreativeMenuToggle].Right = reducedMotion ? TerramonPointID.Party0 : TerramonPointID.PartyCollapse;
                     }
                     else
                     {
-                        invPage.LinkMap[41].Down = 9607;
-                        invPage.LinkMap[311].Right = 9607;
+                        invPage.LinkMap[41].Down = TerramonPointID.HubUI;
+                        invPage.LinkMap[GamepadPointID.CreativeMenuToggle].Right = TerramonPointID.HubUI;
                     }
                 }
                 else
-                    invPage.LinkMap[40].Down = 9607;
+                    invPage.LinkMap[40].Down = TerramonPointID.HubUI;
 
                 //Set trash slot left input if auto trash isn't installed
                 if (!hasAutoTrash)
                 {
                     if (collapseButtonPoint == 48)
-                        invPage.LinkMap[300].Left = 9606;
+                        invPage.LinkMap[GamepadPointID.TrashItem].Left = TerramonPointID.PartyCollapse;
                     else
-                        invPage.LinkMap[300].Left = 9605;
+                        invPage.LinkMap[GamepadPointID.TrashItem].Left = TerramonPointID.Party5;
                 }
             }
         };
@@ -96,23 +96,24 @@ public class UILinkManager : ILoadable
         //TODO: actually add these interactions
         
         //add party slots
-        for (int i = 9600; i <= 9605; i++) {
+        for (int i = TerramonPointID.Party0; i <= TerramonPointID.Party5; i++) {
             UILinkPoint newPoint = new UILinkPoint(i, enabled: true, i - 1, i + 1, 43 + (i - 9600 - slotOffset), -1);
             partyPage.LinkMap.Add(i, newPoint);
         }
         
         //add navigation for party end slots
         if (!hasAutoTrash)
-            partyPage.LinkMap[9605].Right = 300;
+            partyPage.LinkMap[TerramonPointID.Party5].Right = GamepadPointID.TrashItem;
         
         //add other inventory buttons (collapse button and pokedex button)
-        partyPage.LinkMap.Add(9606, new UILinkPoint(9606, enabled: true, 9607, 9600, -1, -1));
-        partyPage.LinkMap[9606].OnSpecialInteracts += () => PlayerInput.BuildCommand(Lang.misc[53].Value, false,
-            PlayerInput.ProfileGamepadUI.KeyStatus["MouseLeft"]);
+        partyPage.LinkMap.Add(TerramonPointID.PartyCollapse, new UILinkPoint(TerramonPointID.PartyCollapse, enabled: true, TerramonPointID.HubUI, -1, -1, -1));
+        partyPage.LinkMap[TerramonPointID.PartyCollapse].OnSpecialInteracts += () => PlayerInput.BuildCommand(Lang.misc[53].Value, false,
+            PlayerInput.ProfileGamepadUI.KeyStatus["MouseLeft"]); //select
         
-        partyPage.LinkMap.Add(9607, new UILinkPoint(9607, enabled: true, -1, reducedMotion ? 9600 : 9606, -1, -1));
-        partyPage.LinkMap[9607].OnSpecialInteracts += () => PlayerInput.BuildCommand(Lang.misc[53].Value, false,
-            PlayerInput.ProfileGamepadUI.KeyStatus["MouseLeft"]);
+        partyPage.LinkMap.Add(TerramonPointID.HubUI, new UILinkPoint(TerramonPointID.HubUI, enabled: true,
+            -1, reducedMotion ? TerramonPointID.Party0 : TerramonPointID.PartyCollapse, -1, -1));
+        partyPage.LinkMap[TerramonPointID.HubUI].OnSpecialInteracts += () => PlayerInput.BuildCommand(Lang.misc[53].Value, false,
+            PlayerInput.ProfileGamepadUI.KeyStatus["MouseLeft"]); //select
         
         //add update events for new link points (anything that needs changing during runtime)
         partyPage.UpdateEvent += delegate
@@ -123,12 +124,13 @@ public class UILinkManager : ILoadable
             //have to set position in case gui scale has changed (even though slot position doesn't)
             for (int i = 0; i <= 5; i++)
             {
+                //not using TerramonPointID here as it would be lengthy + wouldn't improve readability anyways
                 partyPage.LinkMap[9600 + i].Position =
                     (FirstSlotPos + new Vector2(48 * (3 + i - slotOffset), 0)) * Main.UIScale;
                 
                 //add navigation to bestiary button
                 if (i >= 4)
-                    partyPage.LinkMap[9600 + i].Down = 310;
+                    partyPage.LinkMap[9600 + i].Down = GamepadPointID.BestiaryMenu;
                 else //we need to reset this in case it was modified by other methods (the PC)
                     partyPage.LinkMap[9600 + i].Down = -1;
                 
@@ -162,44 +164,45 @@ public class UILinkManager : ILoadable
             }
 
             //left party end slot (might nav to journey button)
-            partyPage.LinkMap[9600].Left = !reducedMotion ? 9606 : Main.GameModeInfo.IsJourneyMode ? 311 : 9607;
+            partyPage.LinkMap[TerramonPointID.Party0].Left = 
+                !reducedMotion ? TerramonPointID.PartyCollapse : Main.GameModeInfo.IsJourneyMode ? GamepadPointID.CreativeMenuToggle : TerramonPointID.HubUI;
 
             //collapse button position + navigation
             var collapseButtonOffset = compressedState || reducedMotion ? 6 : 0;
             if (hasAutoTrash) collapseButtonOffset =+ 1;
             
-            partyPage.LinkMap[9606].Position = (FirstSlotPos + new Vector2(48 * (2 + collapseButtonOffset), 0)) * Main.UIScale;
+            partyPage.LinkMap[TerramonPointID.PartyCollapse].Position = (FirstSlotPos + new Vector2(48 * (2 + collapseButtonOffset), 0)) * Main.UIScale;
 
             if (compressedState || reducedMotion)
             {
-                partyPage.LinkMap[9606].Down = 310;
+                partyPage.LinkMap[TerramonPointID.PartyCollapse].Down = GamepadPointID.BestiaryMenu;
                 if (hasAutoTrash)
                 {
-                    partyPage.LinkMap[9606].Right = -1;
-                    partyPage.LinkMap[9606].Up = 47;
+                    partyPage.LinkMap[TerramonPointID.PartyCollapse].Right = -1;
+                    partyPage.LinkMap[TerramonPointID.PartyCollapse].Up = 47;
                 }
                 else
                 {
-                    partyPage.LinkMap[9606].Right = 300;
-                    partyPage.LinkMap[9606].Up = 48;
+                    partyPage.LinkMap[TerramonPointID.PartyCollapse].Right = GamepadPointID.TrashItem;
+                    partyPage.LinkMap[TerramonPointID.PartyCollapse].Up = 48;
                 }
             }
             else
             {
-                partyPage.LinkMap[9606].Down = -1;
-                partyPage.LinkMap[9606].Right = 9600;
-                partyPage.LinkMap[9606].Up = 42;
+                partyPage.LinkMap[TerramonPointID.PartyCollapse].Down = -1;
+                partyPage.LinkMap[TerramonPointID.PartyCollapse].Right = TerramonPointID.Party0;
+                partyPage.LinkMap[TerramonPointID.PartyCollapse].Up = 42;
             }
 
             //pokedex icon
             var dexOffset = new Vector2(Main.GameModeInfo.IsJourneyMode && !hasAutoTrash ? 1 : 0, Main.GameModeInfo.IsJourneyMode && hasAutoTrash ? 1 : 0);
-            partyPage.LinkMap[9607].Position = (FirstSlotPos + dexOffset * 48) * Main.UIScale;
+            partyPage.LinkMap[TerramonPointID.HubUI].Position = (FirstSlotPos + dexOffset * 48) * Main.UIScale;
             
-            partyPage.LinkMap[9607].Right = reducedMotion && !compressedState ? 9600 : 9606;
-            partyPage.LinkMap[9607].Up = hasAutoTrash && Main.GameModeInfo.IsJourneyMode ? 311 : 40;
+            partyPage.LinkMap[TerramonPointID.HubUI].Right = reducedMotion && !compressedState ? TerramonPointID.Party0 : TerramonPointID.PartyCollapse;
+            partyPage.LinkMap[TerramonPointID.HubUI].Up = hasAutoTrash && Main.GameModeInfo.IsJourneyMode ? GamepadPointID.CreativeMenuToggle : 40;
         };
         
-        UILinkPointNavigator.RegisterPage(partyPage, 2700);
+        UILinkPointNavigator.RegisterPage(partyPage, TerramonPageID.Party);
     }
 
     private static void SetupPCUIPage()
@@ -216,7 +219,7 @@ public class UILinkManager : ILoadable
         for (int x = 0; x <= 5; x++) {
             for (int y = 0; y <= 4; y++)
             {
-                var pointID = 9610 + (x + y * 6);
+                var pointID = TerramonPointID.PC0 + (x + y * 6);
                 UILinkPoint newPoint =
                     new UILinkPoint(pointID, enabled: true, pointID - 1, pointID + 1, pointID - 6, pointID + 6);
                 if (x == 0)
@@ -226,7 +229,7 @@ public class UILinkManager : ILoadable
                 if (y == 5)
                     newPoint.Down = -1;
                 if (y == 0)
-                    newPoint.Up = 9600 + x;
+                    newPoint.Up = TerramonPointID.Party0 + x;
                 if (reducedMotion)
                     newPoint.Position -= new Vector2(48, 0);
                 pcPage.LinkMap.Add(pointID, newPoint);
@@ -239,7 +242,7 @@ public class UILinkManager : ILoadable
             {
                 for (int y = 0; y <= 4; y++)
                 {
-                    var pointID = 9610 + (x + y * 6);
+                    var pointID = TerramonPointID.PC0 + (x + y * 6);
                     
                     //have to set this in UpdateEvent in case gui scale changes
                     pcPage.LinkMap[pointID].Position = (FirstSlotPos + ((new Vector2(3, 1) + new Vector2(x, y)) * 48)) * Main.UIScale;
@@ -266,14 +269,14 @@ public class UILinkManager : ILoadable
             }
         };
         
-        UILinkPointNavigator.RegisterPage(pcPage, 2701);
+        UILinkPointNavigator.RegisterPage(pcPage, TerramonPageID.PC);
 
         //Add navigation to the PC from the party slots
-        var partyPage = UILinkPointNavigator.Pages[2700];
+        var partyPage = UILinkPointNavigator.Pages[TerramonPageID.Party];
         partyPage.UpdateEvent += delegate
         {
             if (!PCInterface.Active) return;
-            for (int i = 9600; i <= 9605; i++)
+            for (int i = TerramonPointID.Party0; i <= TerramonPointID.Party5; i++)
                 partyPage.LinkMap[i].Down = i + 10;
         };
         
@@ -282,12 +285,60 @@ public class UILinkManager : ILoadable
         {
             //modify trash slot hint if a Pokémon is held within the PC
             if (PCInterface.Active && TooltipOverlay.GetHeldPokemon(out var source) != null)
-                invPage.LinkMap[300].OnSpecialInteracts += () => PlayerInput.BuildCommand(
+                invPage.LinkMap[GamepadPointID.TrashItem].OnSpecialInteracts += () => PlayerInput.BuildCommand(
                     Language.GetTextValue("Mods.Terramon.ControllerHints.Release"), false,
                     PlayerInput.ProfileGamepadUI.KeyStatus["MouseLeft"]);
             else
-                invPage.LinkMap[300].OnSpecialInteracts += () => PlayerInput.BuildCommand(Lang.misc[74].Value, false,
+                invPage.LinkMap[GamepadPointID.TrashItem].OnSpecialInteracts += () => PlayerInput.BuildCommand(Lang.misc[74].Value, false,
                     PlayerInput.ProfileGamepadUI.KeyStatus["MouseLeft"]);
         };
     }
+}
+
+public static class TerramonPageID
+{
+    public const int Party = 2700;
+    public const int PC = 2701;
+}
+
+public static class TerramonPointID
+{
+    public const int Party0 = 9600;
+    public const int Party1 = 9601;
+    public const int Party2 = 9602;
+    public const int Party3 = 9603;
+    public const int Party4 = 9604;
+    public const int Party5 = 9605;
+    public const int PartyCollapse = 9606;
+    public const int HubUI = 9607;
+    public const int PC0 = 9610;
+    public const int PC1 = 9611;
+    public const int PC2 = 9612;
+    public const int PC3 = 9613;
+    public const int PC4 = 9614;
+    public const int PC5 = 9615;
+    public const int PC6 = 9616;
+    public const int PC7 = 9617;
+    public const int PC8 = 9618;
+    public const int PC9 = 9619;
+    public const int PC10 = 9620;
+    public const int PC11 = 9621;
+    public const int PC12 = 9622;
+    public const int PC13 = 9623;
+    public const int PC14 = 9624;
+    public const int PC15 = 9625;
+    public const int PC16 = 9626;
+    public const int PC17 = 9627;
+    public const int PC18 = 9628;
+    public const int PC19 = 9629;
+    public const int PC20 = 9630;
+    public const int PC21 = 9631;
+    public const int PC22 = 9632;
+    public const int PC23 = 9633;
+    public const int PC24 = 9634;
+    public const int PC25 = 9635;
+    public const int PC26 = 9636;
+    public const int PC27 = 9637;
+    public const int PC28 = 9638;
+    public const int PC29 = 9639;
 }
