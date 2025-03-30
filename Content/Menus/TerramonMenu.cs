ec8027a5e183fb63481c4f182cd0ab815b4db486
JamzOhJamz <abstractdeveloperx@gmail.com>
2025-03-30 17:46:59 -0600

    Add corruption menu logo

--- Content/Menus/TerramonMenu.cs
+++ Content/Menus/TerramonMenu.cs
@@ -3,30 +3,46 @@ using ReLogic.Content;
 
 namespace Terramon.Content.Menus;
 
+[Autoload(Side = ModSide.Client)]
 public class TerramonMenu : ModMenu
 {
+    private static readonly Asset<Texture2D> LogoPurity;
+    private static readonly Asset<Texture2D> LogoCorruption;
+
+    static TerramonMenu()
+    {
+        if (Main.dedServ) return;
+
+        LogoPurity = ModContent.Request<Texture2D>("Terramon/Assets/Misc/MenuLogo_0");
+        LogoCorruption = ModContent.Request<Texture2D>("Terramon/Assets/Misc/MenuLogo_1");
+    }
+
     public override string DisplayName => "Terramon Mod";
 
     public override int Music => MusicLoader.GetMusicSlot(Mod, "Sounds/Music/TitleTheme");
-
-    public override Asset<Texture2D> Logo =>
-        ModContent.Request<Texture2D>("Terramon/Assets/Misc/MenuLogo");
 
     public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation,
         ref float logoScale,
         ref Color drawColor)
     {
-        /*var logoTexture = Logo.Value;
-        var b = (byte)((255 + Main.tileColor.R * 2) / 3);
-        var color = new Color(b, b, b, 255);
+        // Offsets the logo draw position a bit for alignment purposes
         logoDrawCenter.Y += 16;
-        spriteBatch.Draw(logoTexture, logoDrawCenter, new Rectangle(0, 0, logoTexture.Width, logoTexture.Height),
-            color, logoRotation, new Vector2(logoTexture.Width * 0.5f, logoTexture.Height * 0.5f), logoScale,
-            SpriteEffects.None, 0f);
-        return false;*/
 
-        logoDrawCenter.Y += 16;
-        return true;
+        // Get the logo textures to draw
+        var logoPurity = LogoPurity.Value;
+        var logoCorruption = LogoCorruption.Value;
+
+        // Calculate rectangle and origin for the logo drawing for reuse
+        var logoRect = new Rectangle(0, 0, logoPurity.Width, logoPurity.Height);
+        var logoOrigin = new Vector2(logoPurity.Width * 0.5f, logoPurity.Height * 0.5f);
+
+        // Draws the logo textures depending on the current logo alpha values
+        spriteBatch.Draw(logoPurity, logoDrawCenter, logoRect, drawColor * (Main.LogoA / 255f), logoRotation,
+            logoOrigin, logoScale, SpriteEffects.None, 0f);
+        spriteBatch.Draw(logoCorruption, logoDrawCenter, logoRect, drawColor * (Main.LogoB / 255f), logoRotation,
+            logoOrigin, logoScale, SpriteEffects.None, 0f);
+
+        return false;
     }
 
     /// <summary>
