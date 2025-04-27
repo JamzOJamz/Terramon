e42f8b0d095f911ad4773ccf3ed5ce8632e45a4f
JamzOhJamz <abstractdeveloperx@gmail.com>
2025-04-26 23:34:37 -0600

    More stable progress

--- Content/GUI/StarterSelectUI.cs
+++ Content/GUI/StarterSelectUI.cs
@@ -2,7 +2,9 @@ using ReLogic.Content;
 using Terramon.Content.GUI.Common;
 using Terramon.Core.Loaders.UILoading;
 using Terramon.ID;
+using Terraria.Audio;
 using Terraria.GameContent.UI.Elements;
+using Terraria.Localization;
 using Terraria.UI;
 
 namespace Terramon.Content.GUI;
@@ -10,6 +12,7 @@ namespace Terramon.Content.GUI;
 public sealed class StarterSelectUI : SmartUIState
 {
     private readonly UIStarterBanner[] _banners = new UIStarterBanner[3];
+    private readonly LocalizedText _hintLocalizedText = Language.GetText("Mods.Terramon.GUI.Starter.Hint");
 
     private readonly ushort[] _starters =
     [
@@ -17,6 +20,14 @@ public sealed class StarterSelectUI : SmartUIState
         NationalDexID.Charmander,
         NationalDexID.Squirtle
     ];
+
+    private readonly LocalizedText _subtitleLocalizedText = Language.GetText("Mods.Terramon.GUI.Starter.Subtitle");
+    private readonly LocalizedText _titleLocalizedText = Language.GetText("Mods.Terramon.GUI.Starter.Title");
+    private BetterUIText _hintText;
+    private float _hintTextAlpha;
+    private ITweener _hintTextTween;
+
+    private UIContainer _topContainer;
 
     public override bool Visible =>
         !Main.playerInventory && !Main.inFancyUI && !Main.LocalPlayer.dead &&
@@ -30,27 +41,93 @@ public sealed class StarterSelectUI : SmartUIState
 
     public override void OnInitialize()
     {
-        var topContainer = new UIContainer(new Vector2(362, 210))
+        _topContainer = new UIContainer(new Vector2(372, 314))
         {
             HAlign = 0.5f
         };
-        topContainer.Top.Set(Main.screenHeight / 4f - 105, 0f);
+        var topContainerHeight = Main.screenHeight / 4f - 157;
+        if (Main.screenHeight > 1200) topContainerHeight = Main.screenHeight / 2f - 314 - 143;
+        _topContainer.Top.Set(topContainerHeight, 0f);
+
+        var backdropImage = new UIImage(
+            ModContent.Request<Texture2D>("Terramon/Assets/GUI/Starter/Backdrop"))
+        {
+            RemoveFloatingPointsFromDrawPosition = true,
+            Color = Color.White * 0.3f
+        };
+        backdropImage.Width.Set(1028, 0f);
+        backdropImage.Height.Set(589, 0f);
+        backdropImage.Top.Set(-146, 0f);
+        backdropImage.Left.Set(-329, 0f);
+        _topContainer.Append(backdropImage);
+
+        var titleText = new BetterUIText(_titleLocalizedText)
+        {
+            RemoveFloatingPointsFromDrawPosition = true,
+            TextColor = new Color(232, 241, 255)
+        };
+        var subText = new BetterUIText(_subtitleLocalizedText)
+        {
+            RemoveFloatingPointsFromDrawPosition = true,
+            TextColor = new Color(232, 241, 255)
+        };
+        titleText.HAlign = 0.5f;
+        subText.Top.Set(26, 0);
+        subText.HAlign = 0.5f;
+        titleText.Append(subText);
+        _topContainer.Append(titleText);
+
+        var generationText = new BetterUIText(
+            "Generation I (Kanto)", 0.55f, true)
+        {
+            RemoveFloatingPointsFromDrawPosition = true,
+            ShadowSpread = 1.88f,
+            HAlign = 0.5f
+        };
+        generationText.Top.Set(91, 0f);
+        _topContainer.Append(generationText);
 
         for (var i = 0; i < _banners.Length; i++)
         {
             var banner = new UIStarterBanner(_starters[i]);
             _banners[i] = banner;
-            banner.Top.Set(56, 0f);
-            banner.Left.Set(i * 126, 0f);
-            topContainer.Append(banner);
+            banner.Top.Set(130, 0f);
+            banner.Left.Set(i * 128, 0f);
+            _topContainer.Append(banner);
         }
 
-        Append(topContainer);
+        Append(_topContainer);
+
+        _hintText = new BetterUIText(_hintLocalizedText, 0.85f)
+        {
+            HAlign = 0.5f,
+            TextColor = new Color(173, 173, 198),
+            RemoveFloatingPointsFromDrawPosition = true
+        };
+        _hintText.Top.Set(Main.screenHeight / 2f + 96, 0f);
+        Append(_hintText);
     }
 
     public override void SafeUpdate(GameTime gameTime)
     {
+        var topContainerHeight = Main.screenHeight / 4f - 157;
+        if (Main.screenHeight > 1200) topContainerHeight = Main.screenHeight / 2f - 314 - 143;
+        _topContainer.Top.Set(topContainerHeight, 0f);
+
+        _hintText.Top.Set(Main.screenHeight / 2f + 96, 0f);
+
+        if (_hintTextTween is not { IsRunning: true })
+            _hintTextTween = Tween.To(() => _hintTextAlpha, a => _hintTextAlpha = a, _hintTextAlpha == 1f ? 0f : 1f,
+                1f);
+
         Recalculate();
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        _hintText.TextColor = Color.Lerp(new Color(173, 173, 198), new Color(135, 135, 163), _hintTextAlpha);
+
+        base.Draw(spriteBatch);
     }
 }
 
@@ -59,14 +136,19 @@ internal sealed class UIStarterBanner : UIHoverImageButton
     private static readonly Asset<Texture2D> BackgroundTexture;
     private static readonly Asset<Texture2D> BackgroundHoverTexture;
     private static readonly Asset<Texture2D> ShadowTexture;
+    private readonly ushort _id;
     private readonly UIImage _miniTexture;
+    private readonly BetterUIText _nameText;
     private readonly UIImage _shadow;
+    private readonly BetterUIText _speciesText;
+    private readonly BetterUIText _suffixText;
+    private int _hoverTextModTimeLeft;
     private int _jumpCount;
     private int _jumpTime;
-    private bool _nameGrowing = true;
-    private readonly BetterUIText _nameText;
 
-    private ITweener _nameTween;
+    private Vector2? _lastMousePosition;
+    private int _lastXDirection;
+    private int _shakeCount;
 
     static UIStarterBanner()
     {
@@ -77,44 +159,48 @@ internal sealed class UIStarterBanner : UIHoverImageButton
         ShadowTexture = ModContent.Request<Texture2D>("Terramon/Assets/GUI/Starter/Shadow");
     }
 
-    public UIStarterBanner(ushort id) : base(BackgroundTexture, string.Empty)
+    public UIStarterBanner(ushort id) : base(BackgroundTexture, "Pick this one!")
     {
+        _id = id;
+
         RemoveFloatingPointsFromDrawPosition = true;
-        Width.Set(110, 0f);
-        Height.Set(154, 0f);
+        Width.Set(114, 0f);
+        Height.Set(184, 0f);
         SetHoverImage(BackgroundHoverTexture);
         SetVisibility(1f, 1f);
 
-        _nameText = new BetterUIText(Terramon.DatabaseV2.GetLocalizedPokemonName(id), 0.97f)
+        _nameText = new BetterUIText(Terramon.DatabaseV2.GetLocalizedPokemonName(id), 0.98f)
         {
             RemoveFloatingPointsFromDrawPosition = true,
             HAlign = 0.5f,
+            TextOriginX = 0.5f,
             TextOriginY = 0.5f
         };
+        _nameText.Width.Set(120, 0f);
         _nameText.Height.Set(30, 0f);
-        _nameText.Top.Set(7, 0f);
+        _nameText.Top.Set(10, 0f);
         Append(_nameText);
 
         var species = Terramon.DatabaseV2.GetPokemonSpeciesDirect(id);
         var speciesSplit = species.Split(' ');
         var speciesMod = string.Join(" ", speciesSplit.Take(speciesSplit.Length - 1));
         var suffix = speciesSplit.Last();
-        var speciesText = new BetterUIText(speciesMod, 0.85f)
+        _speciesText = new BetterUIText(speciesMod, 0.85f)
         {
             RemoveFloatingPointsFromDrawPosition = true,
             HAlign = 0.5f,
             TextColor = new Color(232, 241, 255)
         };
-        speciesText.Top.Set(105, 0f);
-        Append(speciesText);
-        var suffixText = new BetterUIText(suffix, 0.85f)
+        _speciesText.Top.Set(109, 0f);
+        Append(_speciesText);
+        _suffixText = new BetterUIText(suffix, 0.85f)
         {
             RemoveFloatingPointsFromDrawPosition = true,
             HAlign = 0.5f,
             TextColor = new Color(232, 241, 255)
         };
-        suffixText.Top.Set(122, 0f);
-        Append(suffixText);
+        _suffixText.Top.Set(126, 0f);
+        Append(_suffixText);
 
         _shadow = new UIImage(ShadowTexture)
         {
@@ -137,23 +223,48 @@ internal sealed class UIStarterBanner : UIHoverImageButton
     public override void Update(GameTime gameTime)
     {
         base.Update(gameTime);
+
+        if (_hoverTextModTimeLeft > 0)
+        {
+            _hoverTextModTimeLeft--;
+            if (_hoverTextModTimeLeft == 0) SetHoverText("Pick this one!");
+        }
+
         if (ContainsPoint(Main.MouseScreen))
         {
-            if (_nameTween is not { IsRunning: true })
+            _lastMousePosition ??= Main.MouseScreen;
+
+            var xDistance = Main.MouseScreen.X - _lastMousePosition.Value.X;
+
+            // Detect horizontal movement of the mouse over the banner
+            if (_hoverTextModTimeLeft == 0 && Math.Abs(xDistance) > 8.5f)
             {
-                _nameTween = Tween.To(() => _nameText.TextScale, _nameText.SetTextScale, _nameGrowing ? 1.1f : 0.97f,
-                    1f / 3f);
-                _nameGrowing = !_nameGrowing;
+                if ((xDistance > 0 && _lastXDirection < 0) || (xDistance < 0 && _lastXDirection > 0)) _shakeCount++;
+
+                // Trigger the Pokémon's cry sound effect! :)
+                if (_shakeCount > 12)
+                {
+                    _shakeCount = 0;
+                    _hoverTextModTimeLeft = 150;
+                    var cry = new SoundStyle("Terramon/Sounds/Cries/" + Terramon.DatabaseV2.GetPokemonName(_id))
+                        { Volume = 0.2525f };
+                    SetHoverText(Terramon.DatabaseV2.GetLocalizedPokemonName(_id) + GetRandomHoverText());
+                    SoundEngine.PlaySound(cry);
+                }
+
+                _lastXDirection = xDistance > 0 ? 1 : -1;
             }
 
+            _lastMousePosition = Main.MouseScreen;
+
             _jumpTime++;
-            if (_jumpTime > 32)
+            if (_jumpTime > 28)
             {
                 _jumpCount++;
                 if (_jumpCount > 1)
                 {
                     _jumpCount = 0;
-                    _jumpTime = -85;
+                    _jumpTime = -75;
                 }
                 else
                 {
@@ -162,25 +273,63 @@ internal sealed class UIStarterBanner : UIHoverImageButton
             }
 
             if (_jumpTime < 0) return;
-            var jumpHeight = GravitySim(_jumpTime / 4f);
+            var jumpHeight = GravitySim(_jumpTime / 3.5f);
             var shadowScale = MathHelper.Lerp(0.85f, 1f, (jumpHeight - 28f) / 8f);
             _miniTexture.Top.Set(jumpHeight, 0f);
             _shadow.ImageScale = shadowScale;
         }
         else
         {
-            if (_nameTween is { IsRunning: true }) _nameTween.Kill();
+            _lastMousePosition = null;
+            _lastXDirection = 0;
+            _shakeCount = 0;
             _jumpTime = 0;
             _jumpCount = 0;
             _miniTexture.Top.Set(36, 0f);
             _shadow.ImageScale = 1f;
             _nameText.SetTextScale(0.97f);
-            _nameGrowing = true;
         }
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        if (ContainsPoint(Main.MouseScreen))
+        {
+            _speciesText.Top.Set(119, 0f);
+            _suffixText.Top.Set(136, 0f);
+        }
+        else
+        {
+            _speciesText.Top.Set(109, 0f);
+            _suffixText.Top.Set(126, 0f);
+        }
+
+        _speciesText.Recalculate();
+        _suffixText.Recalculate();
+        base.Draw(spriteBatch);
     }
 
     private static float GravitySim(float x)
     {
         return 0.5f * x * x - 4f * x + 36f;
+    }
+
+    private static string GetRandomHoverText()
+    {
+        string[] starterTexts =
+        [
+            " seems ready to get started!",
+            " looks like it’s in a good mood!",
+            " is full of excitement!",
+            " seems curious about you!",
+            " looks like it’s waiting for something!",
+            " seems to have a spark of energy!",
+            " is bouncing with excitement!",
+            " looks like it’s ready to play!",
+            " is eager to show you what it can do!",
+            " looks ready to impress!"
+        ];
+
+        return starterTexts[Main.rand.Next(starterTexts.Length)];
     }
 }
