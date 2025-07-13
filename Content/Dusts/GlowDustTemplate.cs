5c438f6462fc242527efa231d67d0f94ce8fa164
GraeDevs <cadenfrizellgaming@gmail.com>
2025-07-12 22:58:51 -0500

    dust and shader stuffs

---
+++ Content/Dusts/GlowDustTemplate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terramon.Content.Dusts
+{
+    public class GlowDustTemplate : ModDust
+    {
+        public override string Texture => "Terramon/Assets/Dusts/GlowDustTemplate";
+
+        public override void OnSpawn(Dust dust)
+        {
+            dust.noGravity = false;
+            dust.noLight = false;
+            dust.frame = new Rectangle(0, 0, 64, 64);
+            dust.color.R = 0;
+            dust.color.G = 0;
+            dust.color.B = 255;
+            dust.shader = new Terraria.Graphics.Shaders.ArmorShaderData(new Ref<Effect>(Terramon.Instance.Assets.Request<Effect>("Effects/GlowingDust", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value), "GlowingDustPass");
+        }
+
+        public override Color? GetAlpha(Dust dust, Color lightColor)
+        {
+            return dust.color;
+        }
+
+        public override bool Update(Dust dust)
+        {
+            if (dust.customData is null)
+            {
+                dust.position -= Vector2.One * 32 * dust.scale;
+                dust.customData = true;
+            }
+
+            Vector2 currentCenter = dust.position + Vector2.One.RotatedBy(dust.rotation) * 32 * dust.scale;
+
+            dust.scale *= 0.95f;
+            Vector2 nextCenter = dust.position + Vector2.One.RotatedBy(dust.rotation + 0.06f) * 32 * dust.scale;
+
+            dust.rotation += 0.06f;
+            dust.position += currentCenter - nextCenter;
+
+            dust.shader?.UseColor(dust.color);
+
+            dust.position += dust.velocity;
+
+            if (!dust.noGravity)
+                dust.velocity.Y += 0.1f;
+
+            dust.velocity *= 0.99f;
+            dust.color *= 0.95f;
+
+            if (!dust.noLight)
+                Lighting.AddLight(dust.position, dust.color.ToVector3());
+
+            if (dust.scale < 0.05f)
+                dust.active = false;
+
+            return false;
+        }
+    }
+
+}
+
---
+++ Effects/ShaderLoader.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
+using Terraria.Graphics.Shaders;
+using Terraria.Graphics.Effects;
+
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using static Terraria.ModLoader.Core.TmodFile;
+using Mono.Cecil;
+
+namespace Terramon.Effects
+{
+    interface OrderedLoadable
+    {
+        void Load();
+        void Unload();
+        float Priority { get; }
+    }
+
+    class ShaderLoader : OrderedLoadable
+    {
+        public float Priority => 0.9f;
+
+        public void Load()
+        {
+            if (Main.dedServ)
+                return;
+
+            MethodInfo info = typeof(Mod).GetProperty("File", BindingFlags.NonPublic | BindingFlags.Instance).GetGetMethod(true);
+            var file = (TmodFile)info.Invoke(Terramon.Instance, null);
+
+            System.Collections.Generic.IEnumerable<FileEntry> shaders = file.Where(n => n.Name.StartsWith("Effects/") && n.Name.EndsWith(".xnb"));
+
+            foreach (FileEntry entry in shaders)
+            {
+                string name = entry.Name.Replace(".xnb", "").Replace("Effects/", "");
+                string path = entry.Name.Replace(".xnb", "");
+                LoadShader(name, path);
+            }
+        }
+
+        public void Unload()
+        {
+
+        }
+
+        public static void LoadShader(string name, string path)
+        {
+            var screenRef = new Ref<Effect>(Terramon.Instance.Assets.Request<Effect>(path, ReLogic.Content.AssetRequestMode.ImmediateLoad).Value);
+            Filters.Scene[name] = new Filter(new ScreenShaderData(screenRef, name + "Pass"), EffectPriority.High);
+            Filters.Scene[name].Load();
+        }
+    }
+}
