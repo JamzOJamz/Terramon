320c66c535082923ae2d8773842348c63b7548ca
JamzOJamz <abstractdeveloperx@gmail.com>
2025-09-16 14:35:07 -0600

    Fix Wikithis cross-mod support

--- Core/Systems/CrossModSystem.cs
+++ Core/Systems/CrossModSystem.cs
@@ -4,7 +4,7 @@ public sealed class CrossModSystem : ModSystem
 {
     public static Mod Wikithis { get; private set; }
 
-    public override void PostSetupContent()
+    public override void OnModLoad()
     {
         if (Main.dedServ)
             return;
@@ -12,7 +12,7 @@ public sealed class CrossModSystem : ModSystem
         // Wikithis compatibility
         if (!ModLoader.TryGetMod("Wikithis", out var wikithis)) return;
         Wikithis = wikithis;
-        wikithis.Call(0, this, "https://terrariamods.wiki.gg/wiki/Terramon_Mod/{}");
-        wikithis.Call(3, this, ModContent.Request<Texture2D>("Terramon/icon_small"));
+        wikithis.Call(0, Mod, "https://terrariamods.wiki.gg/wiki/Terramon_Mod/{}");
+        wikithis.Call(3, Mod, ModContent.Request<Texture2D>("Terramon/icon_small"));
     }
 }
