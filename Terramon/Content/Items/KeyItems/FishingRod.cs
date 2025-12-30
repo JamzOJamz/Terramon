59a3826be1cf3a2d1ffd193ba890d6e7cf5bb6b8
James Z <abstractdeveloperx@gmail.com>
2025-12-29 22:04:43 -0700

    Update FishingRod.cs

--- Terramon/Content/Items/KeyItems/FishingRod.cs
+++ Terramon/Content/Items/KeyItems/FishingRod.cs
@@ -1,63 +1,74 @@
 ﻿using ReLogic.Content;
 using Terramon.Content.Items.PokeBalls;
-using Terraria.GameContent.UI.Chat;
 using Terraria.Localization;
 
-namespace Terramon.Content.Items.KeyItems;
+namespace Terramon.Content.Items;
 
-// i am mr. oop
 public abstract class FishingRod : KeyItem
 {
     /// <summary>
     ///     The default chance for any non-Terramon rod to catch a Terramon item or NPC.
     /// </summary>
     public const float DefaultCatchTerramonChance = 0.0625f;
-    public Asset<Texture2D> InventorySprite;
+
+    private Asset<Texture2D> _inventorySprite;
+
     public float CatchTerramonChance;
+
     public override void SetStaticDefaults()
     {
         base.SetStaticDefaults();
-        InventorySprite = ModContent.Request<Texture2D>(Texture + "_Inventory");
+        _inventorySprite = ModContent.Request<Texture2D>(Texture + "_Inventory");
     }
+
     public override void SetDefaults()
     {
         base.SetDefaults();
-        Item.CloneDefaults(ItemID.WoodFishingPole);
-        Item.maxStack = 1;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.useAnimation = 8;
+        Item.useTime = 8;
+        Item.UseSound = SoundID.Item1;
         Item.shoot = PokeBallBobber.ProjectileType;
-        InventorySprite = ((FishingRod)ItemLoader.GetItem(Type)).InventorySprite;
+        _inventorySprite = ((FishingRod)ItemLoader.GetItem(Type))._inventorySprite;
     }
+
     public override void ModifyFishingLine(Projectile bobber, ref Vector2 lineOriginOffset, ref Color lineColor)
     {
-        // vanilla fix: subtract player's gfxOffY
+        // Vanilla fix: subtract player's gfxOffY
         lineOriginOffset.Y -= Main.player[bobber.owner].gfxOffY;
     }
-    public sealed override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+
+    public sealed override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame,
+        Color drawColor, Color itemColor, Vector2 origin, float scale)
     {
-        spriteBatch.Draw(InventorySprite.Value, position, null, drawColor, 0f, InventorySprite.Size() * 0.5f, scale, 0, 0f);
+        spriteBatch.Draw(_inventorySprite.Value, position, null, drawColor, 0f, _inventorySprite.Size() * 0.5f, scale,
+            0,
+            0f);
         return false;
     }
 }
 
 public sealed class FishingRodGlobal : GlobalItem
 {
-    public static LocalizedText TerramonChanceTooltip { get; private set; }
+    private static LocalizedText TerramonChanceTooltip { get; set; }
+
     public override void SetStaticDefaults()
     {
         TerramonChanceTooltip = Mod.GetLocalization("CommonTooltips.TerramonFishChance");
     }
+
     public override bool AppliesToEntity(Item entity, bool lateInstantiation)
         => lateInstantiation && entity.fishingPole > 0;
+
     public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
     {
         var index = tooltips.FindIndex(l => l.Name is "FishingPower");
         if (index == -1)
             return;
         float fishingChance;
-        if (item.shoot == PokeBallBobber.ProjectileType)
-            fishingChance = ((FishingRod)item.ModItem).CatchTerramonChance;
-        else
-            fishingChance = FishingRod.DefaultCatchTerramonChance;
+        fishingChance = item.shoot == PokeBallBobber.ProjectileType
+            ? ((FishingRod)item.ModItem).CatchTerramonChance
+            : FishingRod.DefaultCatchTerramonChance;
         var line = new TooltipLine
         (
             Mod,
